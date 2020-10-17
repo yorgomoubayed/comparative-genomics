99da08345299e54ff0f751be5b460dc4d9566ee8
Maroua <maroua.chahdil@etu.univ-amu.fr>
2020-10-17 12:59:03 +0000

    Update ftp_cog.py

--- ftp_cog.py
+++ ftp_cog.py
@@ -3,37 +3,37 @@
 """
 Created on Mon Oct 12 10:47:41 2020
 
-@author: marwa
 """
-
+#use counter which will help to count the COG
 from collections import Counter
 
 
-#FTP
+#FTP file
 f=open("/Users/marwa/Desktop/M2 /TODOOOOO/GEGO/projet/python_script/COG.csv","r")
 
-f.readline() #utile pour FTP pas pour eggnog
+#skip the first line of this file because it doesn't provide significiant informations
+f.readline() 
 
+#put the file in a list 
 LEFICHIER = f.readlines()
 col_interet=[]
 
+#loops on the list (which containt the file)
 for ligne in LEFICHIER:
+    #it's a csv file so we split using ;
     header = ligne.split(';')
+    #the COG information is on the fourth colunm
     col_interet.append(header[3].strip())
     #print(type(header[3]))
     
     
 #print(type(int_convert))
-print("taille : ",len(col_interet)) #+23 car certaine proteine sont dans plrs categorie de fonction
-print(col_interet)
-print("heelok")
-
-
-#C  	 G  	 E  	 F  	 H  	 I  	 P  	 Q  	 R  	 S 
-# J  	 A  	 K  	 L  	 B  	 D  	 Y  	 V  	 T  	 M  	 N  	 Z  	 W  	 U  	 O  	 X  	
+print("Lenght : ",len(col_interet)) #+23 car certaine proteine sont dans plrs categorie de fonction
+#print(col_interet)
 
 
 print("Mesoplasma_florum")  
+#return a dictionnary of the number of COG key = COG categorie, values= their number
 print(Counter(col_interet),'\n\n')
 
 
