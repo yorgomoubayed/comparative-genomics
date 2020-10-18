d9e1ffc5b05b7fa68598745de251c71f5ece3b23
Yorgo EL MOUBAYED <yorgo.el-moubayed@etu.univ-amu.fr>
2020-10-18 23:26:09 +0000

    Update eggnog_cog.py

--- eggnog_cog.py
+++ eggnog_cog.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 12 10:47:41 2020
 
-"""
-#use counter which will help to count the COG
+#count the COG functional categories
 from collections import Counter
 
-#EGGnog file
+#eggNog file
 f=open("/Users/marwa/Desktop/M2/TODOOOOO/GEGO/projet/python_script/eggnog_COG.csv", "r")
 
-#put the file in a list 
+#store the file in a list 
 LEFICHIER = f.readlines()
-#create 5 list for our 5 genomes
+
+#create 5 lists for the 5 genomes
 Mesoplasma_florum = Mesoplasma_chauliocola= Mesoplasma_coleopterae= list()
 Entomoplasma_somnilux = Entomoplasma_melaleucae = list()
 
 for ligne in LEFICHIER:
-    #it's a csv file so we split using ;
+    #split using ;
     header = ligne.split(';')
-    #cvs file containt only COG information in each columns we have differents genomes 
+    #csv file containt only COG information in each columns we have differents genomes 
     Mesoplasma_florum.append(header[0].strip())
     Mesoplasma_coleopterae.append(header[1].strip())
     Mesoplasma_chauliocola.append(header[2].strip())
     Entomoplasma_melaleucae.append(header[3].strip())
     Entomoplasma_somnilux.append(header[4].strip())
 
-#Counter :return a dictionnary of the number of COG key = COG categorie, values= their number
+#Counter: return a dictionnary of the number of COG key = COG category, values = associated number
 print("Mesoplasma_florum")  
 print(Counter(Mesoplasma_florum),'\n\n')
 
