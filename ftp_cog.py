52e8f03c485f543170a74f8e85d4e6cbabb2a726
Yorgo EL MOUBAYED <yorgo.el-moubayed@etu.univ-amu.fr>
2020-10-18 23:28:33 +0000

    Update ftp_cog.py

--- ftp_cog.py
+++ ftp_cog.py
@@ -1,40 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 12 10:47:41 2020
 
-"""
-#use counter which will help to count the COG
+#count the COG functional categories
 from collections import Counter
 
+#ftp file
+f=open("/Users/marwa/Desktop/M2/TODOOOOO/GEGO/projet/python_script/COG.csv","r")
 
-#FTP file
-f=open("/Users/marwa/Desktop/M2 /TODOOOOO/GEGO/projet/python_script/COG.csv","r")
-
-#skip the first line of this file because it doesn't provide significiant informations
+#skip the first line of this file because it doesn't provide significant information
 f.readline() 
 
 #put the file in a list 
 LEFICHIER = f.readlines()
 col_interet=[]
 
-#loops on the list (which containt the file)
+#loops on the list (which containts the file)
 for ligne in LEFICHIER:
-    #it's a csv file so we split using ;
+    #split using ;
     header = ligne.split(';')
     #the COG information is on the fourth colunm
     col_interet.append(header[3].strip())
     #print(type(header[3]))
     
-    
 #print(type(int_convert))
-print("Lenght : ",len(col_interet)) #+23 car certaine proteine sont dans plrs categorie de fonction
+print("Lenght : ",len(col_interet)) #+23 car certaines proteines sont dans plrs categories de fonctions
 #print(col_interet)
 
+print("Mesoplasma_florum") 
 
-print("Mesoplasma_florum")  
-#return a dictionnary of the number of COG key = COG categorie, values= their number
+#return a dictionnary of the number of COG key = COG category, values = associated number
 print(Counter(col_interet),'\n\n')
-
-
-
